61f6dae2553e67c19e7641b4b613dbffe559c38a
Joe Quattrone <joequattrone5@gmail.com>
2018-10-24 20:31:28 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,6 +2,18 @@ require 'pry'
 
 class Hash
   def keys_of(*arguments)
+<<<<<<< HEAD
      map {|key, value| arguments.include?(value) ? key : nil }.compact
+=======
+    array = []
+    keys_of.each do |key, value|
+              binding.pry
+
+      if value == arguments
+        return (key)
+      end
+    end
+  
+>>>>>>> a7cdb1cfc3d670e1c55706053cd3ec72a5d5d95d
   end
 end
